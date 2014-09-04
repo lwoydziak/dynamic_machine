13b88dd9e468bf9c27c06adb70c42c43539e181a
Luke <github@woydziak.com>
2014-09-04 15:30:25 -0700

    Bumb github download link revision

--- setup.py
+++ setup.py
@@ -19,11 +19,11 @@ if os.path.exists('README.md'):
 scripts = glob.glob("application/*")
 
 setup(name='DynamicMachine',
-      version='0.2.1',
+      version='0.2.2',
       maintainer='Luke Woydziak',
       maintainer_email='lwoydziak@gmail.com',
       url = 'https://github.com/Pipe-s/dynamic_machine',
-      download_url = 'https://github.com/Pipe-s/dynamic_machine/tarball/1.0',
+      download_url = 'https://github.com/Pipe-s/dynamic_machine/tarball/0.2.2',
       platforms = ["any"],
       description = 'Python package for running the Dynamic Machine application.',
       long_description = read('README.txt'),
