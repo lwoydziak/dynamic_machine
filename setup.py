d682e726728dff2f0131f70a16ccba98a9028a6e
Luke <github@woydziak.com>
2014-09-08 11:38:56 -0700

    fixed dependency

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if os.path.exists('README.md'):
 scripts = glob.glob("application/*")
 
 setup(name='DynamicMachine',
-      version='0.2.3',
+      version='0.2.4',
       maintainer='Luke Woydziak',
       maintainer_email='lwoydziak@gmail.com',
       url = 'https://github.com/Pipe-s/dynamic_machine',
@@ -42,7 +42,8 @@ setup(name='DynamicMachine',
       install_requires=[
                         "apache-libcloud",
                         "pexpect",
-                        "jsonconfigfile"
+                        "jsonconfigfile",
+			"pertinosdk"
                        ],
       scripts=scripts
       )
