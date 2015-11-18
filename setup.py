3a3837d9aa2581ffea0b4650821e585c44a0e7e6
Luke Woydziak <lwoydziak@users.noreply.github.com>
2015-11-17 22:13:47 -0800

    Bump revision

--- setup.py
+++ setup.py
@@ -19,11 +19,11 @@ if os.path.exists('README.md'):
 scripts = glob.glob("application/*")
 
 setup(name='DynamicMachine',
-      version='0.3.2',
+      version='0.4.0,
       maintainer='Luke Woydziak',
       maintainer_email='lwoydziak@gmail.com',
       url = 'https://github.com/Pipe-s/dynamic_machine',
-      download_url = 'https://github.com/Pipe-s/dynamic_machine/tarball/0.3.2',
+      download_url = 'https://github.com/Pipe-s/dynamic_machine/tarball/0.4.0',
       platforms = ["any"],
       description = 'Python package for running the Dynamic Machine application.',
       long_description = read('README.txt'),
@@ -43,7 +43,7 @@ setup(name='DynamicMachine',
                         "apache-libcloud",
                         "pexpect",
                         "jsonconfigfile",
-			            "pertinosdk"
+			"pertinosdk"
                        ],
       scripts=scripts
       )
