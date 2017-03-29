432ada57c911fbcd5761849d697b6529b29c9a97
Luke Woydziak <luke.woydziak@gmail.com>
2017-03-29 16:36:24 -0700

    Update to 0.4.6

--- setup.py
+++ setup.py
@@ -19,11 +19,11 @@ if os.path.exists('README.md'):
 scripts = glob.glob("application/*")
 
 setup(name='DynamicMachine',
-      version='0.4.6',
+      version='0.4.7',
       maintainer='Luke Woydziak',
       maintainer_email='lwoydziak@gmail.com',
       url = 'https://github.com/Pipe-s/dynamic_machine',
-      download_url = 'https://github.com/Pipe-s/dynamic_machine/tarball/0.4.6',
+      download_url = 'https://github.com/Pipe-s/dynamic_machine/tarball/0.4.7',
       platforms = ["any"],
       description = 'Python package for running the Dynamic Machine application.',
       long_description = read('README.txt'),
